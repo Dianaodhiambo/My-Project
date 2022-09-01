19c937611600d52e45731ea0bc178db87ef51402
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-09-01 13:58:24 +0300

    create new column

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const store=[
+    {
+      itemName: 'SuperFood',
+      price: 'Ksh.149.90',
+      image: "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/15/271414/1.jpg?7568"
+    },
+    {
+      itemName: "Janna Product Sesame Seeds 900G",
+      price: "Ksh.148.00",
+      image: "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/56/914274/1.jpg?3373"
+    },
+    {
+      itemName: "ALINA NATURE cofee",
+      price: "Ksh.199.00",
+      image: "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/12/410735/1.jpg?8415"
+    },
+    {
+      itemName: "ALINA NATURE grain",
+      price: "Ksh.99.00",
+      image: "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/89/553735/1.jpg?1516"
+    },
+]
