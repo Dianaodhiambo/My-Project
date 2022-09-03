81b13381d6925865d806d599a3088306a63716fe
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-09-03 19:51:04 +0300

    add data.js

--- js/index.js
+++ js/index.js
@@ -6,16 +6,17 @@ const options = {
 	}
 };
 
-fetch("https://jumia-service.p.rapidapi.com/")
+fetch('https://jumia-service.p.rapidapi.com/api/product/search/grain/4', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
+
   grain.forEach((item, index,)=> {
     const imageContainer = document.createElement('div');
     imageContainer.setAttribute('class', 'col-3 text-center');
-const itemList = document.querySelector("#item-list");
 
+const itemList = document.querySelector("#item-list");
     const img = document.createElement('img'); 
     img.setAttribute('src', `${item.image}`);
     imageContainer.appendChild(img);
@@ -39,7 +40,6 @@ const itemList = document.querySelector("#item-list");
     imageContainer.appendChild(p);
     imageContainer.appendChild(qty);
     imageContainer.appendChild(addToCartBtn);
-
     itemList.appendChild(imageContainer)
 })
 document.querySelectorAll('.add-to-cart').forEach(item => {
@@ -52,3 +52,4 @@ document.addEventListener('DOMContentLoaded', async()=> {
   products = await getGrain();
   updateUI(grain);
 })
+
